e427055ed2e47ba8e68421180aa3a37396acc9bd
Vic P <vic4key@gmail.com>
2022-02-27 16:38:08 +0700

    turn on/off when loading/unloading

--- USB-Relay/USB-Relay.cpp
+++ USB-Relay/USB-Relay.cpp
@@ -31,6 +31,13 @@ USB_RELAY_API void VExt_Load()
 
   bool succeed = RelayManager::instance().initialize();
   VExt::API::log(vu::format_A("Initialize USB Relay %s", succeed ? "succeed" : "failed"));
+
+  auto& devices = RelayManager::instance().list_devices();
+  if (!devices.empty())
+  {
+    RelayManager::instance().select_device(devices[0]);
+    RelayManager::instance().set_state(0, state_t::on);
+  }
 }
 
 USB_RELAY_API void VExt_Unload()
@@ -39,6 +46,8 @@ USB_RELAY_API void VExt_Unload()
   {
     UnregisterSuspendResumeNotification(g_hpn);
   }
+
+  RelayManager::instance().set_state(0, state_t::off);
 
   bool succeed = RelayManager::instance().destroy();
   VExt::API::log(vu::format_A("Uninitialize USB Relay %s", succeed ? "succeed" : "failed"));
